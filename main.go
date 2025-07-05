b15ce86940cb17039c66031703ab7b87f619bea0
TakayukiHirano117 <ikusa.hirano@icloud.com>
2025-07-05 19:55:45 +0900

    init

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"gin-fleamarket/controllers"
+	"gin-fleamarket/models"
+	"gin-fleamarket/repositories"
+	"gin-fleamarket/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	items := []models.Item{
+		{
+			ID:          1,
+			Name:        "Item 1",
+			Price:       1000,
+			Description: "Description 1",
+			SoldOut:     false,
+		},
+		{
+			ID:          2,
+			Name:        "Item 2",
+			Price:       2000,
+			Description: "Description 2",
+			SoldOut:     false,
+		},
+		{
+			ID:          3,
+			Name:        "Item 3",
+			Price:       3000,
+			Description: "Description 3",
+			SoldOut:     false,
+		},
+	}
+
+	itemRepository := repositories.NewItemMemoryRepository(items)
+	itemService := services.NewItemService(itemRepository)
+	itemController := controllers.NewItemController(itemService)
+
+	router := gin.Default()
+	router.GET("/items", itemController.FindAll)
+	router.Run("localhost:8080") // 0.0.0.0:8080 でサーバーを立てます。
+}
