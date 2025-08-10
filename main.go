2dffb65809c6856711d895dcf733578d017e07b0
TakayukiHirano117 <ikusa.hirano@icloud.com>
2025-08-10 14:21:33 +0900

    item作成処理を実装

--- main.go
+++ main.go
@@ -18,28 +18,16 @@ func main() {
 			Description: "Description 1",
 			SoldOut:     false,
 		},
-		{
-			ID:          2,
-			Name:        "Item 2",
-			Price:       2000,
-			Description: "Description 2",
-			SoldOut:     false,
-		},
-		{
-			ID:          3,
-			Name:        "Item 3",
-			Price:       3000,
-			Description: "Description 3",
-			SoldOut:     false,
-		},
 	}
 
 	itemRepository := repositories.NewItemMemoryRepository(items)
 	itemService := services.NewItemService(itemRepository)
 	itemController := controllers.NewItemController(itemService)
 
-	router := gin.Default()
-	router.GET("/items", itemController.FindAll)
-	router.GET("/items/:id", itemController.FindById)
-	router.Run("localhost:8080")
+	r:= gin.Default()
+	r.GET("/items", itemController.FindAll)
+	r.GET("/items/:id", itemController.FindById)
+	r.POST("/items", itemController.Create)
+
+	r.Run("localhost:8080")
 }
