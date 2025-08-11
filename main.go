cfbbc4b2cebf728b4a8cd5332c569b0961cb3e3d
TakayukiHirano117 <ikusa.hirano@icloud.com>
2025-08-11 21:23:02 +0900

    ルーターのグルーピング

--- main.go
+++ main.go
@@ -12,26 +12,19 @@ import (
 func main() {
 	infra.Initialize()
 	db := infra.SetupDB()
-	// items := []models.Item{
-	// 	{
-	// 		Name:        "Item 1",
-	// 		Price:       1000,
-	// 		Description: "Description 1",
-	// 		SoldOut:     false,
-	// 	},
-	// }
 
-	// itemRepository := repositories.NewItemMemoryRepository(items)
 	itemRepository := repositories.NewItemRepository(db)
 	itemService := services.NewItemService(itemRepository)
 	itemController := controllers.NewItemController(itemService)
 
 	r := gin.Default()
-	r.GET("/items", itemController.FindAll)
-	r.GET("/items/:id", itemController.FindById)
-	r.POST("/items", itemController.Create)
-	r.PUT("/items/:id", itemController.Update)
-	r.DELETE("/items/:id", itemController.Delete)
+
+	itemRouter := r.Group("/items")
+	itemRouter.GET("/", itemController.FindAll)
+	itemRouter.GET("/:id", itemController.FindById)
+	itemRouter.POST("/", itemController.Create)
+	itemRouter.PUT("/:id", itemController.Update)
+	itemRouter.DELETE("/:id", itemController.Delete)
 
 	r.Run("localhost:8080")
 }
